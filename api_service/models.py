068578155d9d5fe168a7c6eeb9a03e37ec14d1ce
Balwinder Sodhi <sodhi@iitrpr.ac.in>
2022-11-03 16:40:41 +0530

    Updated

--- api_service/models.py
+++ api_service/models.py
@@ -7,9 +7,7 @@ __version__ = "0.1"
 __status__ = "Development"
 """
 
-from email.policy import default
 import logging
-from datetime import datetime as DT
 
 from peewee import *
 from playhouse.mysql_ext import JSONField
@@ -29,10 +27,8 @@ def create_schema():
     logging.info("Creating DB tables")
     with db:
         db.create_tables(
-            [User, RolePermission, SystemSetting, UserProfile,
-            LeaveApplication, LeaveRule, VacationPeriod,
-            AdminDutyCredit, ResearchCredit, AdminActivity, ResearchActivity,
-            WorkRequest, FormAction, HolidayInfo, AuditLog]
+            [SystemSetting, User, DocAction, AuditLog,
+            DocItem, DocField, DocFieldValue, DocType]
         )
         logging.info("DB tables created.")
 
@@ -86,6 +82,7 @@ class SystemSetting(BaseModel):
 class User(BaseModel):
     email = CharField(max_length=100, unique=True)
     role = CharField(max_length=10)
+    org_unit = CharField(max_length=60)
     first_name = CharField(max_length=200, null=True)
     last_name = CharField(max_length=200, null=True)
     is_locked = BooleanField(constraints=[SQL('DEFAULT FALSE')])
@@ -95,207 +92,61 @@ class User(BaseModel):
             self.last_name, self.email)
 
 
-class RolePermission(BaseModel):
-    role = CharField(max_length=40, index=True)
-    perm = CharField(max_length=100, index=True)
-    description = CharField(max_length=150, null=True)
+class DocType(BaseModel):
+    name = CharField(max_length=60, index=True)
+    description = TextField(null=True)
 
     class Meta:
-        indexes = (
-            # Unique index
-            (("role", "perm"), True),
-        )
+        indexes = ()
 
 
-class UserProfile(BaseModel):
-    user = ForeignKeyField(User, backref="profiles")
-    gender = CharField(max_length=10, constraints=[SQL("DEFAULT 'M'")])
-    dob = DateField()
-    doj = DateField(default=DT.now().date)
-    category = CharField(max_length=10, null=True)
-    basic_pay = IntegerField(default=0)
-    cpc_level = CharField(max_length=10, null=True)
-    area = CharField(max_length=150)
-    valid_from = DateField(default=DT.now().date)
-    valid_till = DateField(null=True)
-    allowed_compoff = BooleanField(constraints=[SQL('DEFAULT FALSE')])
+class DocField(BaseModel):
+    doc_type = ForeignKeyField(DocType, backref="doc_fields")
+    name = CharField(max_length=60, index=True)
+    # float, integer, string, date, boolean
+    field_type = CharField(max_length=60)
+    optional = BooleanField(constraints=[SQL('DEFAULT TRUE')])
+    description = TextField(null=True)
 
     class Meta:
-        indexes = (
-            # Unique index
-            (("user", "valid_from", "valid_till",), True),
-        )
+        indexes = ()
 
 
-class LeaveRule(BaseModel):
-    sub_role = CharField(max_length=10)
-    leave_type = CharField(max_length=40)
-    yearly_quota = IntegerField()
-    max_per_instance = IntegerField()
-    max_accrual = IntegerField(constraints=[SQL('DEFAULT 0')])
-    min_service_years = IntegerField(constraints=[SQL('DEFAULT 0')])
-    rolls_over = BooleanField(constraints=[SQL('DEFAULT FALSE')])
-    encashable = BooleanField(constraints=[SQL('DEFAULT FALSE')])
-    fraction_allowed = BooleanField(constraints=[SQL('DEFAULT FALSE')])
-    conversion_to_el = FloatField(constraints=[SQL('DEFAULT 0')])
-    valid_from = DateField()
-    valid_till = DateField(null=True)
-    rec_role = CharField(max_length=10, null=True)
-    apr_role = CharField(max_length=10, constraints=[SQL("DEFAULT 'HOD'")])
-    match_area = BooleanField(constraints=[SQL('DEFAULT FALSE')])
-    
-    class Meta:
-        indexes = (
-            # Unique index
-            (("sub_role", "leave_type", "valid_from"), True),
-        )
-
-
-class VacationPeriod(BaseModel):
-    year = IntegerField()
-    start_dt = DateField()
-    end_dt = DateField()
-    
-    class Meta:
-        indexes = (
-            # Unique index
-            (("year", "start_dt", "end_dt",), True),
-        )
-
-
-class LeaveApplication(BaseModel):
-    user = ForeignKeyField(User, backref="leaves")
-    leave_type = CharField(max_length=40)
-    leaving_stn = BooleanField(default=False)
-    start_dt = DateField()
-    end_dt = DateField()
+class DocItem(BaseModel):
+    doc_type = ForeignKeyField(DocType, backref="doc_items")
     status = CharField(max_length=40)
-    leave_phone = CharField(max_length=20)
-    leave_addr = TextField()
-    applicant_notes = TextField()
-    approver_notes = TextField(null=True)
-    reco_notes = TextField(null=True)
-    fn_an = CharField(max_length=4, default="NA")
-
-    class Meta:
-        constraints = [Check('start_dt <= end_dt')]
-        indexes = (
-            # Unique index
-            (("user", "start_dt", "status",), True),
-            (("user", "end_dt", "status",), True),
-        )
-
-
-class AdminDutyCredit(BaseModel):
-    work_type = CharField(max_length=200)
-    credits = IntegerField()
-    status = CharField(max_length=40)
-    valid_from = DateField()
-    valid_till = DateField(null=True)
-    
-    def to_string(self):
-        return "{0} (Cr. {1})".format(self.work_type, self.credits)
+    # UUID value
+    doc_file = CharField(max_length=50)
     
     class Meta:
         indexes = (
             # Unique index
-            (("work_type", "valid_from", "valid_till",), True),
+            (("doc_type", "status", "doc_file"), True),
         )
 
 
-class ResearchCredit(BaseModel):
-    classif = CharField(max_length=200)
-    pub_type = CharField(max_length=200)
-    credits = IntegerField()
-    status = CharField(max_length=40)
-    valid_from = DateField()
-    valid_till = DateField(null=True)
-
-    def to_string(self):
-        return "{0} ({1}, Cr. {2})".\
-            format(self.classif, self.pub_type, self.credits)
+class DocFieldValue(BaseModel):
+    doc_field = ForeignKeyField(DocField, backref="field_values")
+    doc_item = ForeignKeyField(DocItem, backref="field_values")
+    field_val = CharField(max_length=150, null=True)
 
     class Meta:
         indexes = (
             # Unique index
-            (("classif", "pub_type", "valid_from", "valid_till",), True),
+            (("doc_field", "doc_item"), True),
         )
 
 
-class AdminActivity(BaseModel):
-    user = ForeignKeyField(User, backref="admin_activities")
-    credit = ForeignKeyField(AdminDutyCredit, backref="credit_actitivies")
-    credit_override = IntegerField(null=True)
-    status = CharField(max_length=40, default="DRA")
-    start_dt = DateField()
-    end_dt = DateField(null=True)
-
-    class Meta:
-        indexes = (
-            # Unique index
-            (("user", "credit", "start_dt",), True),
-        )
-
-
-class ResearchActivity(BaseModel):
-    user = ForeignKeyField(User, backref="research_activities")
-    credit = ForeignKeyField(ResearchCredit, backref="credit_actitivies")
-    credit_override = IntegerField(null=True)
-    status = CharField(max_length=40, default="DRA")
-    title = CharField(max_length=255)
-    authors = TextField()
-    pub_name = TextField()
-    pub_dt = DateField(null=True)
-    # A = Accepted, P = Published
-    pub_status = CharField(max_length=2)
-    doi = CharField(max_length=255, null=True, unique=True)
-
-    class Meta:
-        indexes = (
-            # Unique index
-            (("user", "credit", "title", "pub_dt"), True),
-        )
-
-
-class WorkRequest(BaseModel):
-    user = ForeignKeyField(User, backref="work_requests")
-    status = CharField(max_length=40)
-    start_dt = DateField()
-    end_dt = DateField()
-    reasons = TextField()
-    approver_notes = TextField(null=True)
-
-    class Meta:
-        indexes = (
-            # Unique index
-            (("user", "start_dt",), True),
-        )
-
-
-class HolidayInfo(BaseModel):
-    start_dt = DateField()
-    end_dt = DateField()
-    period_type = CharField(max_length=10)
-    description = CharField(max_length=250)
-    
-    class Meta:
-        indexes = (
-            # Unique index
-            (("start_dt", "end_dt",), True),
-        )
-
-
-class FormAction(BaseModel):
-    form = CharField(max_length=100)
+class DocAction(BaseModel):
+    doc_type = ForeignKeyField(DocType, backref="doc_actions")
     user_role = CharField(max_length=10)
     status_now = CharField(max_length=40)
     action = CharField(max_length=60)
     status_after = CharField(max_length=40, null=True)
-    match_area = BooleanField(constraints=[SQL('DEFAULT TRUE')])
+    allowed_ou = CharField(max_length=60)
 
     class Meta:
         indexes = (
             # Unique index
-            (("form", "status_now", "user_role", "action"), True),
+            (("doc_type", "user_role", "status_now", "action"), True),
         )
-
